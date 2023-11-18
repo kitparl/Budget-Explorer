3722b787946d7fe6745e7583e80b7c61fee94a55
kitparl <pranshu.java@gmail.com>
2023-11-18 22:45:39 +0530

    add progress

--- frontend/src/app/month/add-expanse/add-expanse.component.ts
+++ frontend/src/app/month/add-expanse/add-expanse.component.ts
@@ -6,43 +6,47 @@ import { SharedDataService } from 'src/services/shared-data.service';
 import { StorageBrowser } from '../../../storage/storage.browser';
 import { ToastrService } from 'ngx-toastr';
 import { BudgetCalculate } from '../../../utilites/budgetCalculate';
+import { MonthExpanses } from '../../../models/MonthExpanses';
 
 
 @Component({
   selector: 'app-add-expanse',
   templateUrl: './add-expanse.component.html',
-  styleUrls: ['./add-expanse.component.css','../../app.component.css'],
+  styleUrls: ['./add-expanse.component.css', '../../app.component.css'],
 })
-export class AddExpanseComponent implements OnInit{
+export class AddExpanseComponent implements OnInit {
   monthData: any;
   isHomeComponent = false;
   form1: FormGroup;
   form2: FormGroup;
   public dataSource = new BehaviorSubject<any>(null);
-  
-  constructor(private router: Router, private sharedDataService: SharedDataService, private formBuilder: FormBuilder, private storageBrowser: StorageBrowser, private toastrService: ToastrService, private budgetCalculate: BudgetCalculate) { 
+  otherExpensesItems: any[];
+
+  constructor(private router: Router, private sharedDataService: SharedDataService, private formBuilder: FormBuilder, private storageBrowser: StorageBrowser, private toastrService: ToastrService, private budgetCalculate: BudgetCalculate) {
     this.form1 = this.formBuilder.group({
       budget: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/)]],
       investment: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/)]],
       saving: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/)]],
-    },{ validator: this.savingAndInvestmentValidator });
+    }, { validator: this.savingAndInvestmentValidator });
 
     this.form2 = this.formBuilder.group({
       expanse: new FormControl(''),
       amount: new FormControl(''),
 
     });
+    this.otherExpensesItems = this.storageBrowser.get("otherExpensesItems") || [];
+    console.log('[ constructir otherExpensesItems ] >', this.otherExpensesItems);
   }
 
   savingAndInvestmentValidator(control: AbstractControl) {
     const budgetControl = control.get('budget');
     const investmentControl = control.get('investment');
     const savingControl = control.get('saving');
-  
+
     if (budgetControl && investmentControl) {
       const budget = budgetControl.value;
       const investment = investmentControl.value;
-  
+
       if (budget != null && investment != null) {
         if (investment > budget * 0.3) {
           investmentControl.setErrors({ investmentExceedsBudget: true });
@@ -55,7 +59,7 @@ export class AddExpanseComponent implements OnInit{
     if (budgetControl && savingControl) {
       const budget = budgetControl.value;
       const saving = savingControl.value;
-  
+
       if (budget != null && saving != null) {
         if (saving > budget * 0.5) {
           savingControl.setErrors({ savingExceedsBudget: true });
@@ -64,41 +68,67 @@ export class AddExpanseComponent implements OnInit{
         }
       }
     }
-  
+
     return null;
   }
-  
+
 
 
   goHome() {
     this.isHomeComponent = !this.isHomeComponent;
     this.router.navigate(['month'])
     const storedPopupState = sessionStorage.clear();
-  
+
   }
 
   ngOnInit(): void {
-        // Subscribe to dataEmitter in SharedDataService
-        this.sharedDataService.dataEmitter.subscribe((data) => {
-        this.storageBrowser.set('monthData', data);
-        });
+    // Subscribe to dataEmitter in SharedDataService
+    this.sharedDataService.dataEmitter.subscribe((data) => {
+      this.storageBrowser.set('monthData', data);
+    });
     sessionStorage.setItem('isPopupOpen', JSON.stringify(true));
     let ans = this.storageBrowser.get('monthData');
-        console.log('[ ans ] >', ans);
-        this.monthData = ans;
+    console.log('[ ans ] >', ans);
+    this.monthData = ans;
   }
 
 
   onSubmit(event: Event) {
     // Submit the form data to the server
+
     const formName = (event.target as HTMLFormElement).name;
     if (formName === 'setBasics') {
       const formValues = this.form1.value;
+      let savedMonthData: MonthExpanses = {
+        id: this.monthData.id,
+        year: this.monthData.year,
+        month: this.monthData.month,
+        budget: formValues.budget,
+        investmentAmount: formValues.investment,
+        savingAmount: formValues.saving,
+        totalExpanseThisMonth: formValues.budget - (formValues.investment + formValues.saving),
+        // otherExpanse: []
+      }
+      this.monthData = savedMonthData;
+      this.storageBrowser.set("newMonthData", formValues)
       console.log('[ formValues ] >', formValues);
-      
+
       this.budgetCalculate.calCulateMainBudget(formValues, this.monthData);
     } else if (formName === 'otherExpanse') {
-      this.budgetCalculate.calculateOtherExpense(this.form2.value);
+      const formValues = this.form2.value;
+      console.log('[ formValues ] >', formValues);
+      // this.budgetCalculate.calculateOtherExpense(this.form2.value);
+
+      console.log("typeof this.otherExpensesItems:", typeof this.otherExpensesItems);
+      console.log("this.otherExpensesItems:", this.otherExpensesItems);
+      this.otherExpensesItems.push(formValues);
+      console.log("After push, this.otherExpensesItems:", this.otherExpensesItems);
+
+      this.storageBrowser.set(
+        "otherExpensesItems",
+        JSON.stringify(
+          this.otherExpensesItems
+        ))
     }
   }
 
--- frontend/src/models/MonthExpanses.ts
+++ frontend/src/models/MonthExpanses.ts
@@ -1,12 +1,12 @@
 import { OtherExpanses } from "./OtherExpanses";
 
 export interface MonthExpanses {
-    id: string,
-    year: number,
-    month: string,
-    budget: number,
-    investmentAmount: number,
-    savingAmount: number,
-    totalExpanseThisMonth: number,
-    otherExpanse: any,
+    id?: string,
+    year?: number,
+    month?: string,
+    budget?: number,
+    investmentAmount?: number,
+    savingAmount?: number,
+    totalExpanseThisMonth?: number,
+    otherExpanse?: any,
 }
